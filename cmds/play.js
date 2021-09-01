30e0926e667f423b4cad4e289f692d08d5c501e8
classidied <claire.wang028@gmail.com>
2021-09-01 13:49:55 -0400

    what

--- cmds/play.js
+++ cmds/play.js
@@ -4,11 +4,6 @@ module.exports = {
     name: 'play' || 'p',
     description: 'play something',
     async execute(message, args) {
-        const { channel } = message.member.voice;
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        });
+        
     }
 }
